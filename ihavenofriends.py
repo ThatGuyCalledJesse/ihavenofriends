635c0a19a5c2970fb32979db1dcefaf01ebe7270
ThatGuyCalledJesse <72275409+ThatGuyCalledJesse@users.noreply.github.com>
2022-10-16 21:11:42 +0200

    Added main file
    This is the file you will run, this is the file

---
+++ ihavenofriends.py
@@ -0,0 +1,10 @@
+import random
+import os
+
+lines = ["I totally agree", "Good, how are you?", "Whales are pretty fucking big", "Jurassic park released in 1993, that's a long time ago", "Hi", "You should try minecraft", "Minecraft is getting camels (or is it cammels?)", "Egypt is a country in Africa", "Counting sheep...", "No", "Yes", "I actually don't know what you are saying", "Firefox has a cool logo", "I want to thank our sponsor, Raid: Shadow Legends", "Subnautica good game", "Fun fact: this isn't a fun fact", "FEED THE PENGUINS", "Deinosuchus was pretty big", "I have no friends", "Programming is fun", "Yeah, your way of doing it is better"]
+
+os.system("clear")
+
+while True:
+	input(">")
+	print(random.choice(lines))
